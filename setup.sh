57d028c21f8383052c857e8a01fb19ae9bfab9a5
Christoph Daum <christoph.daum@coding-pioneers.com>
2026-04-05 00:28:53 +0200

    fix(setup): reset CHANGELOG.md for derived projects
    Closes #26

--- setup.sh
+++ setup.sh
@@ -270,6 +270,24 @@ else
     info "No remaining placeholders found."
 fi
 
+# --- Reset CHANGELOG ---
+
+info "Resetting CHANGELOG.md..."
+cat > CHANGELOG.md <<'CHANGELOG_EOF'
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [Unreleased]
+
+### Added
+
+- Initial project setup
+CHANGELOG_EOF
+
 # --- Clean up ---
 
 info "Removing setup script..."
